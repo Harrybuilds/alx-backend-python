b209ff3ab71dda40288a9b7c5511c2511bca0659
Harrison <harrisonchialuka@gmail.com>
2024-10-11 08:59:08 +0100

    task 0 correction

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -4,7 +4,7 @@ module that houses a function that returns the sum of 2 float numbers
 """
 
 
-def add(a:float, b:float) -> float:
+def add(a: float, b: float) -> float:
     """
     function that returns float version
     of suming two numbers
